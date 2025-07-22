576d52fcad0774d58500791d28529b20e7b87dad
ajitesh786 <https://github.com/ajitesh786/File_Handling.git>
2025-07-22 10:06:28 -0700

    Initial commit

--- File_Handling.c/amode.h
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int add(int x,int y) {
-    int c = x  * y;
-    printf("Amode.txt %d\n",c);
-    return c;
-
-}
--- File_Handling.c/try.c
+++
@@ -1,5 +0,0 @@
-#include<stdio.h>
-int main() {
-    printf("I am trying one more chance.");
-    return 0;
-}
